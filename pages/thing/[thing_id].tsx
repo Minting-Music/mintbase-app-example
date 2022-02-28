854ee44fe59f1c7902bd3991508197110bca63f3
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-28 16:42:10 +0100

    fixed design response when connected and ann bottom margin

--- pages/thing/[thing_id].tsx
+++ pages/thing/[thing_id].tsx
@@ -112,15 +112,15 @@ const Product = ({ thing_id }: { thing_id: string }) => {
             {loadingTokensData && <Loader />}
 
             {!loadingTokensData &&
-                <main className="h-screen py-24 lg:mt-8">
+                <main className="h-full py-24 mt-4">
                     <div className="container mx-auto md:px-6">
-                        <div className="lg:grid lg:grid-cols-3 h-full justify-center p-3 w-full">
-                            <div className="content-center my-auto rounded-md p-5 lg:col-span-2 border lg:mb-24 mx-4 shadow-2xl  bg-gray-50">
+                        <div className="lg:grid lg:grid-cols-2 justify-center w-full">
+                            <div className="rounded-md lg:col-span-1 border mx-4 my-auto p-2 box-border shadow-2xl bg-gray-50">
 
                                 <>
                                     {!things[0]?.metadata.animation_type &&
                                     <div className="py-2">
-                                        <img className=" object-contain mx-auto w-96 "
+                                        <img className=" object-contain mx-auto w-full my-auto"
                                             src={things[0]?.metadata.media}
                                             alt={things[0]?.metadata.title} />
                                     </div>
@@ -128,7 +128,7 @@ const Product = ({ thing_id }: { thing_id: string }) => {
                                     }
 
                                     {things[0]?.metadata.animation_type &&
-                                        <div id="responsiveVideoWrapper" className="my-8 rounded-md">
+                                        <div id="responsiveVideoWrapper" className="rounded-md my-auto">
                                             <Player src={things[0]?.metadata.animation_url!} thumbnail={things[0]?.metadata.media} size={"big"}></Player>
                                         </div>
                                         
@@ -137,9 +137,9 @@ const Product = ({ thing_id }: { thing_id: string }) => {
 
 
                             </div>
-                            <div className="mb-16 priceTag content-center mx-4 mb-24 xl:mb-2 md:mb-12 lg:mb-6">
+                            <div className="priceTag content-center mx-4 col-span-1 mb-4">
 
-                                <div className='xl:max-w-lg lg:max-w-md ' id='container'>
+                                <div className='' id='container'>
                                     <div className='app-border mb-2'>
                                         <h3 className="text-gray-700 uppercase text-lg font-bold">{things[0]?.metadata.title}</h3>
                                         <p className='text-gray-400 py-2'>Owned by: <span className='storeID'>{things[0]?.store.name}</span> </p>
@@ -147,7 +147,7 @@ const Product = ({ thing_id }: { thing_id: string }) => {
                                     <div className='app-border'>
                                         <DescriptionIcon />
                                         <span className='text-gray-700 text-[18px] pt-2 '>Description</span>
-                                            <p className={hide? 'pt-2 h-16 overflow-y-scroll ': 'pt-2 h-16 overflow-y-scroll truncate'}>
+                                            <p className={hide? 'pt-2 h-24 overflow-y-scroll ': 'pt-2 h-16 overflow-y-scroll truncate'}>
                                                 <span className='storeID'>{things[0]?.metadata.description}</span>
                                             </p>
                                         <span id='span' onClick={play} className='cursor-pointer text-blue-400 p-2'>{!hide ? 'see more' : 'see less'}</span>
@@ -158,7 +158,7 @@ const Product = ({ thing_id }: { thing_id: string }) => {
                                 <div className='text-gray-500 mt-2 text-sm app-border'>
                                     <p className=' p-2'>Store ID: {things[0]?.storeId} </p>
 
-                                    <p ><a className='text-blue-400 p-2' target="_blank" href={`https://explorer.${process.env.NETWORK === 'testnet' ? 'testnet' : ''}.near.org/transactions/${things[0]?.tokens[0].txId}`} rel="noreferrer" >Near Link</a></p>
+                                    <p ><a className='text-blue-400 p-2' target="_blank" href={`https://explorer.${process.env.NETWORK === 'testnet' ? 'testnet' : ''}near.org/transactions/${things[0]?.tokens[0].txId}`} rel="noreferrer" >Near Link</a></p>
 
                                     <p>
                                         <a className='text-blue-400 p-2' href={`https://viewblock.io/arweave/tx/${thing_id.split(":")[0]}`} target="_blank" rel="noreferrer">
@@ -170,38 +170,39 @@ const Product = ({ thing_id }: { thing_id: string }) => {
                                     <p className=' p-2'>Tokens: {things[0]?.tokens.length} out of {things[0]?.allTokens.length}</p>
 
                                     <p><a className='text-blue-400 p-2' href={things[0]?.metadata.external_url} target="_blank" rel="noreferrer">Project Website</a> </p>
-
-
-                                    {isConnected && things[0]?.tokens[0].list.autotransfer &&
-                                        <>
-                                            <div className='xl:pt-10 xl:pb-5 lg:pt-8 lg:pb-5 md:py-5 sm:py-4 '>
+                                    
+                                    <div className='xl:pt-10 xl:pb-5 lg:pt-8 lg:pb-5 md:py-5 sm:py-4 '>
                                                 <span className='text-gray-500 mt-12 text-sm '>current price</span> <br />
                                                 <span className=" text-xl object-contain flex  m-5 justify-start  items-center">
                                                     <img src="../images/near.png" alt="here" className='w-4 h-4 ' />
                                                     <span className='px-2'>{price} </span>
                                                 </span>
                                             </div>
-                                            <div className="flex items-center pt-2 app-border-solid  app-border-t-2 app-border-full app-border-gray-200">
+
+                                    {isConnected && things[0]?.tokens[0].list.autotransfer &&
+                                        <>
+                                            
+                                            <div className="flex items-center pt-2 border-solid  border-t-2 border-full border-gray-200">
                                                 <button className="fontFamily buyButton" onClick={buy}>
                                                     <AccountBalanceWalletIcon className='mr-4' />
                                                     Buy
                                                 </button>
                                             </div>
                                         </>
-                                    }
+                                            }
 
                                     {
                                         isConnected && !things[0]?.tokens[0].list.autotransfer &&
                                         <>
-                                            <div className='xl:pt-14 xl:pb-5 lg:pt-11 lg:pb-5 md:py-5 sm:py-8'>
-                                                <span className='text-gray-500 mt-12 text-sm mx-5'>current Bid</span> <br />
+                                            <div className='flex max-w-md'>
+                                                <span className='text-gray-500 my-auto text-sm mx-5'>current Bid</span> <br />
                                                 <span className=" text-xl flex m-5 justify-start">
-                                                    <img src="../images/near-protocol-near-logo.png" alt="here" className='w-4 h-4 my-auto' />
+                                                    <img src="../images/near.png" alt="here" className='w-4 h-4 my-auto' />
                                                     <span className='px-2'>{currentBid} </span>
                                                 </span>
                                             </div>
                                             <div>
-                                                <input value={bid} type="number" onChange={e => setBid(e.target.value)} min="0" className="rounded-full focus:outline-none text-gray-700 py-2" />
+                                                <input value={bid} type="number" onChange={e => setBid(e.target.value)} min="0" className=" ml-2 pl-4 rounded-full focus:outline-none border text-gray-700 py-2" />
                                             </div>
                                             <div className="flex items-center mt-3">
                                                 <button className="buyButton" onClick={buy}>
@@ -209,7 +210,7 @@ const Product = ({ thing_id }: { thing_id: string }) => {
                                                     Bid</button>
                                             </div>
                                         </>
-                                    }
+                                      }
 
                                 </div>
                             </div>
